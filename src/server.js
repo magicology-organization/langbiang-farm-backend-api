63cae8405f0010c1a28092b2012962fac88b7760
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-10-25 23:55:57 +0700

    added SQL Connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,45 @@
-import express from "express";
+const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello!");
+// Database connection setup
+const connection = mysql.createConnection({
+  host: "database-1.cxvv1ga3c11l.us-east-1.rds.amazonaws.com",
+  user: "anisphia",
+  password: "euphyllia",
+  database: "test_db",
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database: " + err.stack);
+    return;
+  }
+  console.log("Connected to database as ID " + connection.threadId);
+});
+
+// Route to fetch data from the 'users' table
+app.get("/users", (req, res) => {
+  // Query to select all records from the 'users' table
+  const query = "SELECT * FROM users";
+
+  // Execute the query
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error("Error querying database: " + error.stack);
+      res.status(500).send("Error fetching data from the database");
+      return;
+    }
+
+    // Send the results as JSON response
+    res.json(results);
+  });
+});
+
+// Start the Express server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
