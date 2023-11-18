6063a980e1b121f40a4952956da8ebcce1a916c7
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-18 10:04:34 +0700

    please work

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 // server.js
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
+const cors = require("cors");
 
 const app = express();
 
 // Use the productRoutes for all product-related routes
 app.use("/api/products", productRoutes);
+app.use(cors());
 
 module.exports = app;
