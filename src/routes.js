d1eb741cf3ce86d02a13480aa38cb391420edd4c
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-18 11:07:48 +0700

    modified cors

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,10 @@
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const cors = require("cors");
-
+app.use(cors());
 const app = express();
 
 // Use the productRoutes for all product-related routes
 app.use("/api/products", productRoutes);
-app.use(cors());
 
 module.exports = app;
