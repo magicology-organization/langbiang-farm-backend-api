a845fc736070e799dcd3942cf60387f64c826d92
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-11-18 12:59:14 +0700

    fixed cors

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const cors = require("cors");
-app.use(cors());
-const app = express();
 
+const app = express();
+app.use(cors());
 // Use the productRoutes for all product-related routes
 app.use("/api/products", productRoutes);
 
